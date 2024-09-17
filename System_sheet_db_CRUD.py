f1ff7b83e33b6348449231de08c6594e7bccc655
datnt1171 <157688888+datnt1171@users.noreply.github.com>
2024-09-17 11:00:11 +0700

    search by header

--- System_sheet_db_CRUD.py
+++ System_sheet_db_CRUD.py
@@ -7,13 +7,18 @@ Created on Thu Aug 15 10:55:14 2024
 
 import os
 import pandas as pd
+from io import StringIO
+## connect to postgresql
+import psycopg2
+conn = psycopg2.connect(database="system_sheet", user="postgres", password="lkjhgnhI1@", host="localhost", port=5432)
+cur = conn.cursor()
 
 
 main_folder_path = r'D:\VL1251\Ratio_compare\production_process\QUY TRINH MOI'
 
 excel_name_list = []
 excel_sheet_list = []
-excel_text_list = []
+#excel_text_list = []
 excel_path_list = []
 
 
@@ -33,7 +38,7 @@ for root, dirs, files in os.walk(main_folder_path):
                 # Append sheet name
                 excel_sheet_list.append(sheet_name)
                 # Append the DataFrame for the current sheet
-                excel_text_list.append(sheet_data)
+                #excel_text_list.append(sheet_data)
                 # Append the path to the list
                 excel_path_list.append(file_path)
 
@@ -78,25 +83,23 @@ for excel_path, excel_sheet in zip(excel_path_list,excel_sheet_list):
 
 
 
-excel_text_list_csv = [df.to_csv(index=False) for df in excel_text_list]  # Convert each DataFrame to a CSV string
-
-## connect to postgresql
-import psycopg2
-conn = psycopg2.connect(database="system_sheet", user="postgres", password="lkjhgnhI1@", host="localhost", port=5432)
-cur = conn.cursor()
+#excel_text_list_csv = [df.to_csv(index=False) for df in excel_text_list]  # Convert each DataFrame to a CSV string
 
 
-# cur.execute("""DROP TABLE Excel_data;""")
-# cur.execute("""DROP TABLE bug_report;""")
-# cur.execute("""DROP TABLE users;""")
-# conn.commit()
+
+
+cur.execute("""DROP TABLE excel_data;""")
+cur.execute("""DROP TABLE bug_report;""")
+cur.execute("""DROP TABLE users;""")
+conn.commit()
 # Create table
-cur.execute("""CREATE TABLE Excel_data(
-            excel_id SERIAL PRIMARY KEY,
+cur.execute("""CREATE TABLE excel_data(
             excel_name TEXT,
             excel_sheet TEXT,
-            excel_text TEXT,
-            excel_path TEXT);
+            excel_path TEXT,
+            pdf_path TEXT,
+            image_path TEXT,
+            pdf_name TEXT);
             """)
             
 cur.execute(""" CREATE TABLE bug_report(
@@ -113,15 +116,34 @@ cur.execute("""CREATE TABLE users(
             password text
             )""")
 
-# Make the changes to the database persistent
 conn.commit()
 
+
+
+# Create pdf_path and image_path
+col_map = {'excel_name':excel_name_list,
+           'excel_sheet':excel_sheet_list,
+           'excel_path':excel_path_list}
+df = pd.DataFrame(col_map)
+df['pdf_path'] = df['excel_path'].replace('.xlsx','',regex=True)
+df['pdf_path'] = df['pdf_path'].replace('.xls','',regex=True)
+df['pdf_path'] = df['pdf_path'] + "_" + df['excel_sheet'] + '.pdf'
+
+df['image_path'] = df['excel_path'].replace('.xlsx','',regex=True)
+df['image_path'] = df['image_path'].replace('.xls','',regex=True)
+df['image_path'] = df['image_path'] + "_" + df['excel_sheet'] + '.png'
+
+df['pdf_name'] = df['excel_name'].replace('.xlsx','',regex=True)
+df['pdf_name'] = df['pdf_name'].replace('.xls','',regex=True)
+df['pdf_name'] = df['pdf_name'] + "_" + df['excel_sheet'] + '.pdf'
 # Insert data into table
-for name, sheet, text, path in zip(excel_name_list, excel_sheet_list, excel_text_list_csv, excel_path_list):
+
+for excel_name, excel_sheet, excel_path, pdf_path, image_path, pdf_name in \
+zip(df['excel_name'], df['excel_sheet'], df['excel_path'], df['pdf_path'], df['image_path'], df['pdf_name']):
     cur.execute("""
-        INSERT INTO Excel_data (excel_name, excel_sheet, excel_text, excel_path)
-        VALUES (%s, %s, %s, %s);
-    """, (name, sheet, text, path))
+        INSERT INTO excel_data (excel_name, excel_sheet, excel_path, pdf_path, image_path, pdf_name)
+        VALUES (%s, %s, %s, %s, %s, %s);
+    """, (excel_name, excel_sheet, excel_path, pdf_path, image_path, pdf_name))
 conn.commit()
 
 
@@ -138,14 +160,57 @@ cur.execute("""DELETE FROM excel_data WHERE excel_sheet = '空白表格-再修�
 cur.execute("""DELETE FROM excel_data WHERE excel_sheet = '產品編號'""")
 cur.execute("""DELETE FROM excel_data WHERE excel_sheet = 'V'""")
 cur.execute("""DELETE FROM excel_data WHERE excel_sheet = 'Material code'""")
+cur.execute("""DELETE FROM excel_data WHERE excel_sheet = '印-V'""")
+cur.execute("""DELETE FROM excel_data WHERE excel_sheet = '印-中'""")
 # Import user into user table
 cur.execute("INSERT INTO users (username, password) VALUES ('dat', '123')")
 cur.execute("INSERT INTO users (username, password) VALUES ('stanley', '123')")
 cur.execute("INSERT INTO users (username, password) VALUES ('dungtq', '123')")
 
-
+conn.commit()
 cur.close()
 conn.close()
 
 cur.execute("SELECT * from excel_data where excel_text like '%5703%' ;")
 temp = cur.fetchall()
+
+
+
+
+
+# System sheet header
+cur.execute("DROP table system_sheet_header")
+cur.execute("""CREATE TABLE system_sheet_header(
+            pdf_name TEXT,
+            date DATE,
+            factory_name_combined TEXT,
+            panel_code TEXT,
+            paint_system_grouped TEXT,
+            paint_system TEXT,
+            sheen TEXT,
+            distressing TEXT,
+            customber_name TEXT,
+            description TEXT)""")
+conn.commit()
+
+
+system_sheet_header = pd.read_excel(r'D:\VL1251\Ratio_compare\production_process\new_db.xlsx')
+output = StringIO()
+system_sheet_header.to_csv(output, sep='\t', header=False, index=False)
+output.seek(0)
+
+# Define the table name
+table_name = 'system_sheet_header'
+
+try:
+    # Copy data to the table
+    cur.copy_from(output, table_name, sep='\t')
+    conn.commit()
+    print("DataFrame imported to PostgreSQL successfully!")
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    if cur:
+        cur.close()
+    if conn:
+        conn.close()
