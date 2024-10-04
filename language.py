47649d856201e05a011385045b1a51d0f2063db4
datnt1171 <157688888+datnt1171@users.noreply.github.com>
2024-10-04 14:25:30 +0700

    Add files via upload

---
+++ language.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  4 12:53:57 2024
+
+@author: KT1
+"""
+
+translations = {
+    'English': {
+        'select_language':'Select Language',
+        'title': 'System Sheet Search',
+        'company_name': 'Input Company Name',
+        'panel_code': 'Input Panel (Furniture) Code',
+        'show_pdf': 'Show System Sheet',
+        'search': 'Search',
+        'report_bug': 'Report a Bug',
+        'download_excel': 'Download Excel',
+        'no_data': 'No System Sheet Found For This Code',
+        'no_input':'Please Enter a Name/Code to search',
+        'num_found':'Total System Sheet Found:',
+        'characteristic':'System Sheet Characteristics',
+        'no_image':'No Image Preview For This System Sheet',
+        'no_pdf':'No PDF Preview For This System Sheet',
+        'bug_info':'Describe the bug',
+        'email':'Your email (Optional)',
+        'image_upload':'Upload an image (Recommended)',
+        'submit_bug':'Submit Bug Report',
+        'bug_submited':'Bug report submitted successfully. Thank you!',
+        'bug_error':'Failed to submit bug report. Please try again later.',
+        'bug_warning':'Please describe the bug before submitting.'
+    },
+    'Vietnamese': {
+        'select_language':'Chọn ngôn ngữ',
+        'title': 'Tìm Quy Trình',
+        'company_name': 'Chọn tên công ty',
+        'panel_code': 'Nhập mã màu',
+        'show_pdf': 'Hiện quy trình',
+        'search': 'Tìm kiếm',
+        'report_bug': 'Báo cáo lỗi',
+        'download_excel': 'Tải File Excel',
+        'no_data': 'Không tìm thấy quy trình nào cho mã này',
+        'no_input':'Chọn công ty/mã màu',
+        'num_found':'Tổng số quy trình tìm được:',
+        'characteristic':'Đặc điểm của các quy trình',
+        'no_image':'Không có ảnh xem trước cho quy trình này',
+        'no_pdf':'Không có bản xem trước cho quy trình này',
+        'bug_info':'Mô tả lỗi',
+        'email':'Email (Không bắt buộc)',
+        'image_upload':'Ảnh mô tả lỗi (Nên có)',
+        'submit_bug':'Báo lỗi',
+        'bug_submited':'Báo lỗi thành công',
+        'bug_error':'Báo lỗi không thành công. Hãy thử lại sau',
+        'bug_warning':'Nhập mô tả lỗi gặp phải'
+    },
+    'Chinese (Traditional)': {
+        'select_language':'選擇語言',
+        'title': '系統表搜尋',
+        'company_name': '選擇公司',
+        'panel_code': '輸入面板（家具）代碼',
+        'show_pdf': '顯示系統表',
+        'search': '搜尋',
+        'report_bug': '報告錯誤',
+        'download_excel': '下載Excel',
+        'no_data': '未找到此代碼的系統表',
+        'no_input':'請輸入名稱/代碼進行搜尋',
+        'num_found':'找到的總系統表：',
+        'characteristic':'系統表特性',
+        'no_image':'此系統表沒有影像預覽',
+        'no_pdf':'此系統表沒有 PDF 預覽',
+        'bug_info':'描述錯誤',
+        'email':'您的電子郵件（可選）',
+        'image_upload':'上傳圖片（建議）',
+        'submit_bug':'提交錯誤報告',
+        'bug_submited':'錯誤報告提交成功。謝謝你！',
+        'bug_error':'無法提交錯誤報告。請稍後重試。',
+        'bug_warning':'請在提交前描述該錯誤。'
+    }
+    
+}
